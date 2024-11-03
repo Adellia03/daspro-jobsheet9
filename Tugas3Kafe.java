c4c6ff59680288cc8200c6edc0de883605318026
Adellia03 <salsadellia736@gmail.com>
2024-11-04 00:40:12 +0700

    Adellia

---
+++ Tugas3Kafe.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class Tugas3Kafe {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[] menu = { "Nasi Goreng", "Mie Goreng", "Roti Bakar","Kentang Goreng", "Teh Tarik", "Cappucino", "Chocolate Ice"};
+
+        System.out.print("Masukkan nama makanan yang ingin dicari: ");
+        String makananDicari = sc.nextLine();
+        boolean ditemukan = false; 
+
+        for (String makanan : menu) {
+            if (makanan.equalsIgnoreCase(makananDicari)) {
+                ditemukan = true; 
+                break;
+            }
+        }
+
+        if (ditemukan) {
+            System.out.println(makananDicari + " tersedia di menu");
+        } else {
+            System.out.println(makananDicari + " tidak ada di menu");
+        }
+    }
+}
